cce9804f8881d7c726267ea75fdc5dc670dfb2ee
Mainak <8697093687m@gmail.com>
2026-01-11 14:43:25 +0530

    Push the latest changes of the American time Zone

--- SeleniumTraining part2/src/test/java/ui/Navigatecommand.java
+++ SeleniumTraining part2/src/test/java/ui/Navigatecommand.java
@@ -1,10 +1,32 @@
 package ui;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Navigatecommand {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("Mainak");
+        driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("Mainak123");
+        Actions action = new Actions(driver);
+        action.sendKeys(Keys.ENTER).perform();
+        driver.navigate().to("https://rahulshettyacademy.com/AutomationPractice/");
+        WebElement yt = driver.findElement(By.xpath("//input[@id='autocomplete']"));
+        action.contextClick(yt).perform();
 	}
 
 }
